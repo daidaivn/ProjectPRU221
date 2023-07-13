bc2234838ddcdfdbeb2f538b70ed21f29bb259dd
unknown <donghiem114@gmail.com>
2023-07-13 21:24:19 +0700

    create iteam boom

---
+++ Assets/Scripts/Map/ItemCollision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollision : MonoBehaviour
+{
+    public string playerTag = "Player"; // Tag của nhân vật người chơi
+    public string boomTag = "Boom"; // Tag của vật phẩm "Boom"
+    public string enemyTag = "Enemy"; // Tag của các đối tượng Enemy
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Kiểm tra nếu vật phẩm va chạm với đối tượng có tag là "Player"
+        if (collision.CompareTag(playerTag))
+        {
+            // Xóa tất cả các đối tượng có tag "Enemy" trên màn hình
+            DeleteAllEnemies();
+        }
+        // Kiểm tra nếu vật phẩm có tag là "Boom" va chạm với đối tượng có tag là "Player"
+        else if (collision.CompareTag(boomTag))
+        {
+            // Xóa tất cả các đối tượng có tag "Enemy" trên màn hình
+            DeleteAllEnemies();
+            // Xóa chính vật phẩm có tag "Boom"
+            Destroy(gameObject);
+        }
+    }
+
+    private void DeleteAllEnemies()
+    {
+        GameObject[] enemyObjects = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        foreach (GameObject enemyObject in enemyObjects)
+        {
+            if (enemyObject != gameObject)
+            {
+                Destroy(enemyObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Map/ItemSpawner.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public List<GameObject> itemPrefabs; // Danh sách các Prefab của vật phẩm
+    public Transform character; // Transform của nhân vật
+    public float spawnIntervalMin = 3f; // Thời gian tối thiểu giữa các lần sinh vật phẩm
+    public float spawnIntervalMax = 7f; // Thời gian tối đa giữa các lần sinh vật phẩm
+    public float minDistance = 2f; // Khoảng cách tối thiểu giữa vật phẩm và nhân vật
+    public float maxDistance = 10f; // Khoảng cách tối đa giữa vật phẩm và nhân vật
+    public float despawnDistance = 15f; // Khoảng cách thay đổi vị trí vật phẩm
+
+    private float timer; // Đếm thời gian giữa các lần sinh vật phẩm
+    private float spawnInterval; // Thời gian giữa các lần sinh vật phẩm hiện tại
+
+    private List<GameObject> spawnedItems; // Danh sách chứa các vật phẩm đã sinh ra
+
+    private void Start()
+    {
+        spawnedItems = new List<GameObject>();
+        GenerateSpawnInterval(); // Tạo thời gian giữa các lần sinh vật phẩm ban đầu
+    }
+
+    private void Update()
+    {
+        // Đếm thời gian
+        timer += Time.deltaTime;
+
+        // Kiểm tra nếu đến thời điểm sinh vật phẩm mới
+        if (timer >= spawnInterval)
+        {
+            SpawnItem();
+            GenerateSpawnInterval(); // Tạo thời gian giữa các lần sinh vật phẩm mới
+            timer = 0f; // Đặt lại đếm thời gian
+        }
+
+        // Kiểm tra và xóa các vật phẩm cách xa camera
+        for (int i = spawnedItems.Count - 1; i >= 0; i--)
+        {
+            GameObject item = spawnedItems[i];
+            if (Vector3.Distance(item.transform.position, character.position) > despawnDistance)
+            {
+                // Không xóa vật phẩm mà chỉ đặt lại vị trí
+                item.transform.position = GetRandomPosition();
+            }
+        }
+    }
+
+    private void SpawnItem()
+    {
+        // Chọn ngẫu nhiên một Prefab từ danh sách
+        GameObject randomItemPrefab = itemPrefabs[Random.Range(0, itemPrefabs.Count)];
+
+        // Tạo vị trí ngẫu nhiên trong khoảng giới hạn
+        Vector3 randomPosition = GetRandomPosition();
+
+        // Kiểm tra khoảng cách với nhân vật
+        float distance = Vector3.Distance(randomPosition, character.position);
+        if (distance < minDistance)
+        {
+            Vector3 safePosition = character.position + (randomPosition - character.position).normalized * minDistance;
+            randomPosition = safePosition;
+        }
+
+        // Sinh ra vật phẩm tại vị trí đã chọn
+        GameObject newItem = Instantiate(randomItemPrefab, randomPosition, Quaternion.identity);
+        spawnedItems.Add(newItem);
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        Vector3 randomDirection = Random.insideUnitCircle.normalized;
+        Vector3 randomPosition = character.position + randomDirection * Random.Range(minDistance, maxDistance);
+        randomPosition.z = 0f;
+
+        return randomPosition;
+    }
+
+    private void GenerateSpawnInterval()
+    {
+        spawnInterval = Random.Range(spawnIntervalMin, spawnIntervalMax);
+    }
+}
